0a329a86f93d7478a77dea2586d094f2db5bf813
Dakoog <taktoja@spoko.pl>
2022-05-18 13:04:28 +0200

    modify all classes with builder package

--- src/main/java/designPattern/builder/BuilderMain.java
+++ src/main/java/designPattern/builder/BuilderMain.java
@@ -1,4 +1,25 @@
 package designPattern.builder;
 
 public class BuilderMain {
+    public static void main(String[] args) {
+        Hero myHeroOfBonnieTyler = new Hero.Builder()
+                .setName("Super")
+                .setSurname("Sheriff")
+                .setMarried(false)
+                .setDriver(true)
+                .createHero();
+        System.out.println(myHeroOfBonnieTyler);
+        Hero myFamily = new Hero.Builder()
+                .setFatherName("John")
+                .setMotherName("Liza")
+                .setBrotherName("Peter")
+                .setSisterName("Margie")
+                .setGrandmotherName("Barbora")
+                .setCateName("Blue")
+                .setAddress("Pcim Dolny")
+                .createHero();
+        System.out.println(myFamily);
+    }
+
 }
+
--- src/main/java/designPattern/builder/Hero.java
+++ src/main/java/designPattern/builder/Hero.java
@@ -5,11 +5,10 @@ public class Hero {
     private double weight, height, waistCircumference, shoeSize;
     private boolean married, driver;
 
-
-    // constructor for Strings
-    public Hero(String name, String surname, String motherName, String fatherName,
-                String cateName, String sisterName, String brotherName, String
-                        grandmotherName, String address, String school) {
+    private Hero(String name, String surname, String motherName, String fatherName, String cateName,
+                 String sisterName, String brotherName, String grandmotherName, String address,
+                 String school, double weight, double height, double waistCircumference, double shoeSize,
+                 boolean married, boolean driver) {
         this.name = name;
         this.surname = surname;
         this.motherName = motherName;
@@ -20,49 +19,136 @@ public class Hero {
         this.grandmotherName = grandmotherName;
         this.address = address;
         this.school = school;
-
-    }
-
-    // constructor for double
-    public Hero(double weight, double height, double waistCircumference, double shoeSize) {
         this.weight = weight;
         this.height = height;
         this.waistCircumference = waistCircumference;
         this.shoeSize = shoeSize;
-    }
-
-    //boolean constructor
-    public Hero(boolean married, boolean driver) {
         this.married = married;
         this.driver = driver;
     }
 
-    //family constructor
-    public Hero(String name, String surname, String motherName, String fatherName,
-                String sisterName, String brotherName, String grandmotherName) {
-        this.name = name;
-        this.surname = surname;
-        this.motherName = motherName;
-        this.fatherName = fatherName;
-        this.sisterName = sisterName;
-        this.brotherName = brotherName;
-        this.grandmotherName = grandmotherName;
-    }
-    // heroes constructor (only hero data)
-
-    public Hero(String name, String surname, double weight, double height, double waistCircumference, double shoeSize, boolean married, boolean driver) {
-        this.married = married;
-        this.driver = driver;
-        this.weight = weight;
-        this.height = height;
-        this.waistCircumference = waistCircumference;
-        this.shoeSize = shoeSize;
-        this.name = name;
-        this.surname = surname;
+    @Override
+    public String toString() {
+        return "HERO:\n" +
+                " name = " + name + "\n" +
+                " surname = " + surname + "\n" +
+                " motherName = " + motherName + "\n" +
+                " fatherName = " + fatherName + "\n" +
+                " cateName = " + cateName + "\n" +
+                " sisterName = " + sisterName + "\n" +
+                " brotherName = " + brotherName + "\n" +
+                " grandmotherName = " + grandmotherName + "\n" +
+                " address = " + address + "\n" +
+                " school =' " + school + "\n" +
+                " weight = " + weight + "\n" +
+                " height = " + height + "\n" +
+                " waistCircumference = " + waistCircumference + "\n" +
+                " shoeSize = " + shoeSize + "\n" +
+                " married = " + married + "\n" +
+                " driver = " + driver;
     }
 
-    // no args constructor
-    public Hero() {
+    public static class Builder {
 
+        private String name;
+        private String surname;
+        private String motherName;
+        private String fatherName;
+        private String cateName;
+        private String sisterName;
+        private String brotherName;
+        private String grandmotherName;
+        private String address;
+        private String school;
+        private double weight;
+        private double height;
+        private double waistCircumference;
+        private double shoeSize;
+        private boolean married;
+        private boolean driver;
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setSurname(String surname) {
+            this.surname = surname;
+            return this;
+        }
+
+        public Builder setMotherName(String motherName) {
+            this.motherName = motherName;
+            return this;
+        }
+
+        public Builder setFatherName(String fatherName) {
+            this.fatherName = fatherName;
+            return this;
+        }
+
+        public Builder setCateName(String cateName) {
+            this.cateName = cateName;
+            return this;
+        }
+
+        public Builder setSisterName(String sisterName) {
+            this.sisterName = sisterName;
+            return this;
+        }
+
+        public Builder setBrotherName(String brotherName) {
+            this.brotherName = brotherName;
+            return this;
+        }
+
+        public Builder setGrandmotherName(String grandmotherName) {
+            this.grandmotherName = grandmotherName;
+            return this;
+        }
+
+        public Builder setAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder setSchool(String school) {
+            this.school = school;
+            return this;
+        }
+
+        public Builder setWeight(double weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public Builder setHeight(double height) {
+            this.height = height;
+            return this;
+        }
+
+        public Builder setWaistCircumference(double waistCircumference) {
+            this.waistCircumference = waistCircumference;
+            return this;
+        }
+
+        public Builder setShoeSize(double shoeSize) {
+            this.shoeSize = shoeSize;
+            return this;
+        }
+
+        public Builder setMarried(boolean married) {
+            this.married = married;
+            return this;
+        }
+
+        public Builder setDriver(boolean driver) {
+            this.driver = driver;
+            return this;
+        }
+
+        public Hero createHero() {
+            return new Hero(name, surname, motherName, fatherName, cateName, sisterName, brotherName, grandmotherName, address, school, weight, height, waistCircumference, shoeSize, married, driver);
+        }
     }
 }
