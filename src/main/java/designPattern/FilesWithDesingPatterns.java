2f4fa532df4ebd3a3a5c74a9433a330554f0e3cd
Dakoog <taktoja@spoko.pl>
2022-05-29 17:54:50 +0200

    added the factory.txt file with the description of the factor pattern and its use

--- src/main/java/designPattern/FilesWithDesingPatterns.java
+++ src/main/java/designPattern/FilesWithDesingPatterns.java
@@ -5,16 +5,23 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class FilesWithDesingPatterns{
+public class FilesWithDesingPatterns {
     public static void main(String[] args) throws IOException {
-        File singleton = new File("singleton.txt");
-        File builder= new File("builder.txt");
 
+        /*
+        File singleton = new File("singleton.txt");
         createFile(singleton);
         writeToFile(singleton);
 
+        File builder = new File("builder.txt");
         createFile(builder);
         writeToFile(builder);
+        */
+
+        File factory = new File("factory.txt");
+        createFile(factory);
+        writeToFile(factory);
+
 
     }
 
@@ -26,12 +33,17 @@ public class FilesWithDesingPatterns{
 
     private static void writeToFile(File file) throws FileNotFoundException {
         PrintWriter entry = new PrintWriter(file);
-        entry.println("What is this singleton?");
+        entry.println("*** FACTORY ***");
         entry.println("""
-                Singleton ->  There is only one instance of a class
-                     for the entire program, and no other can be created.
-                     The instance is available for entire code -> is public and static!
-                     Warning! Static breaks the principle of object orientation.""");
+                What is this factory pattern
+                Factory ->  Exists in three forms:
+                           * factory method
+                           * abstract factory
+                           * factory
+                           When there is a need to define certain objects or types of objects to be built in some intended way.
+                           There should be objects that are practical and have very specific fields.
+                            
+                      """);
         entry.close();
     }
 }
