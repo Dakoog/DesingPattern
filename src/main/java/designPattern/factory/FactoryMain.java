23fd8293da9c8a9868494534843381db51fbbcc7
Dakoog <taktoja@spoko.pl>
2022-05-29 17:56:31 +0200

    added the factory.txt file with the description of the factor pattern and its use

---
+++ src/main/java/designPattern/factory/FactoryMain.java
@@ -0,0 +1,11 @@
+package designPattern.factory;
+
+public class FactoryMain {
+    public static void main(String[] args) {
+        // this is my order for Lamborghini Service & Shop
+        Car myLamborghini =new Car("Lamborghini", "Veneno Roadster", "goldEdition", 6.5, 100000, 900, 5, true);
+        //reaction -> haha:))), Security, take those guys, please....
+
+
+    }
+}
