df2c7a21ddc4289d941fbe93ff6459e3407b829d
Dakoog <taktoja@spoko.pl>
2022-05-31 10:09:11 +0200

    Modify Factory Main

--- src/main/java/designPattern/factory/FactoryMain.java
+++ src/main/java/designPattern/factory/FactoryMain.java
@@ -1,10 +1,19 @@
 package designPattern.factory;
 
+import designPattern.factory.car.Car;
+
 public class FactoryMain {
     public static void main(String[] args) {
         // this is my order for Lamborghini Service & Shop
-        Car myLamborghini =new Car("Lamborghini", "Veneno Roadster", "goldEdition", 6.5, 100000, 900, 5, true);
+       // Car myLamborghini =new Car("Lamborghini", "Veneno Roadster", "goldEdition", 6.5, 100000, 900, 5, true);
         //reaction -> haha:))), Security, take those guys, please....
+
+        // create own objects is can't because
+        // the access modifier prevents this
+
+      //Car car = new Car("Lamborghini", "Veneno Roadster", "goldEdition", 6.5, 100000, 900, 5, true);
+        Car myLamborghini = Car.createMyLamborghiniVenenoRoadster();
+
 
 
     }
